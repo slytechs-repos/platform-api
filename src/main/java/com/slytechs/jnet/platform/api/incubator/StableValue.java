8561c629890f48a78bec7c1570dde54eaad06741
Mark Bendarczyk <mark@slytechs.com>
2024-12-30 17:20:48 -0500

    Added StableValue into the platform.incubator package, as it will be replaced by the JDK implementation when its in production.

--- src/main/java/com/slytechs/jnet/platform/api/incubator/StableValue.java
+++ src/main/java/com/slytechs/jnet/platform/api/incubator/StableValue.java
@@ -15,7 +15,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package com.slytechs.jnet.platform.api.util;
+package com.slytechs.jnet.platform.api.incubator;
 
 import java.io.Closeable;
 import java.util.function.Supplier;
